235ad22aa929b0a6dce9f381c2c975f4abab60f5
Bruno Lopes <bruno-lopes9@hotmail.com>
2025-11-23 21:26:15 +0000

    Add Category Model

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+
+        // columns we sant in category tables;
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
