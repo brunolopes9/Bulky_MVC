a18bf807b7928f96733f9a7982c0a2eadb80adb9
Bruno Lopes <bruno-lopes9@hotmail.com>
2025-11-24 08:37:32 +0000

    Add category table and web controller and view

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -6,10 +6,10 @@ namespace BulkyWeb.Models
     {
 
         // columns we sant in category tables;
-        [Key]
+        
         public int Id { get; set; }
         [Required]
-        public int Name { get; set; }
+        public string Name { get; set; }
         public int DisplayOrder { get; set; }
 
     }
